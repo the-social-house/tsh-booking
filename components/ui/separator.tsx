8d9fda96619a71cd2bde269ac34e7a760624a4d3
Jacob Friis Strand <hello@jacobfriisstrand.com>
2025-12-05 14:20:02 +0100

    feat: add new UI components, add resend sdk, update dependencies and add nvmrc file (#6)

---
+++ components/ui/separator.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Root as SeparatorPrimitiveRoot } from "@radix-ui/react-separator";
+import type * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+function Separator({
+  className,
+  orientation = "horizontal",
+  decorative = true,
+  ...props
+}: React.ComponentProps<typeof SeparatorPrimitiveRoot>) {
+  return (
+    <SeparatorPrimitiveRoot
+      className={cn(
+        "shrink-0 bg-border data-[orientation=horizontal]:h-px data-[orientation=vertical]:h-full data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-px",
+        className
+      )}
+      data-slot="separator"
+      decorative={decorative}
+      orientation={orientation}
+      {...props}
+    />
+  );
+}
+
+export { Separator };
