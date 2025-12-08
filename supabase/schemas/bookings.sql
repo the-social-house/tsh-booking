7b6a42eca79cd8b0f04cc91c517df557d0adc944
Jonas Blædel <jonasblaedel27@gmail.com>
2025-12-08 09:21:36 +0100

    chore: update package dependencies and scripts, add Supabase integration

---
+++ supabase/schemas/bookings.sql
@@ -0,0 +1,15 @@
+create table bookings (
+  booking_id bigint primary key generated always as identity,
+  booking_user_id bigint not null references users (user_id),
+  booking_meeting_room_id bigint not null references meeting_rooms (meeting_room_id),
+  booking_start_time timestamptz not null,
+  booking_end_time timestamptz not null,
+  booking_created_at timestamptz default now() not null,
+  booking_date date not null,
+  booking_number_of_people int not null,
+  booking_total_price numeric(10, 2) not null,
+  booking_discount numeric(5, 2) default 0,
+  booking_payment_status text not null default 'pending',
+  booking_stripe_transaction_id text,
+  booking_receipt_url text
+);
